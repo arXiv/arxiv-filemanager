cad7d23b74714785de74ff822249c2c753330720
David Fielding <dlf2@cornell.edu>
2018-07-17 13:54:40 -0400

    Create database tables

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 """Provides application for development purposes."""
 
 from filemanager.factory import create_web_app
+from filemanager.services import uploads
 
 app = create_web_app()
+
+uploads.db.create_all()
