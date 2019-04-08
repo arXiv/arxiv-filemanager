8ef7a519825edb0bae8a22d33681283d30842168
erickpeirson <brp53@cornell.edu>
2019-04-08 10:26:15 -0400

    ARXIVNG-2020 set up helm deployment for filemanager service

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,11 @@ from filemanager.factory import create_web_app
 import os
 
 
+__flask_app__ = create_web_app()
+
+
 def application(environ, start_response):
     """WSGI application factory."""
     for key, value in environ.items():
         os.environ[key] = str(value)
-    app = create_web_app()
-    return app(environ, start_response)
+    return __flask_app__(environ, start_response)
